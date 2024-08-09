80eb2a2623493a6f45bba4abf6d04204c247f27a
Shoib Loya <52075633+shoibloya@users.noreply.github.com>
2024-08-10 02:55:59 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,8 @@ import os
 
 if not os.environ.get("TAVILY_API_KEY"):
     os.environ["TAVILY_API_KEY"] = st.secrets['tapiKey']
-
+    os.environ["OPENAI_API_KEY"] = st.secrets['apiKey']
+    
 tools = [TavilySearchResults(max_results=1)]
 
 # Initialize the OpenAI client with your API keys
